3043b20b1af4a5400a311651039f1233e729c684
Aritra Banerjee <103491935+AritraLeo@users.noreply.github.com>
2024-05-08 16:32:49 +0530

    Initial commit before test

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const busboy = require('busboy');
+const AWS = require('aws-sdk');
+const uuidv4 = require('uuid');
+const bodyParser = require('body-parser');
+const { log, error } = require('console');
+const PORT = process.env.PORT || 5500;
+
+
+const app = express();
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_ACCESS_SECRET,
+    region: process.env.AWS_REGION
+});
+
+const s3 = new AWS.S3();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/upload', (req, res) => {
+    const busboyInstance = busboy({headers: req.headers});
+
+    let key = "";
+
+    busboyInstance.on('file', (fieldname, file, filename, encoding, mimetype) => {
+        const key = `uploads/${uuidv4()}_${filename}`;
+
+        const params = {
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: key,
+            Body: file
+        }
+
+        s3.upload(params, (err, data) => {
+            if(err){
+                console.log('Err uploading', err);
+                res.status(500).json({error: 'Internal Server Err'});
+            }else{
+                console.log('File uploaded to', data.Location);
+                res.status(200).json({message: 'File upload done', location: data.Location});
+            }
+        });
+    });
+
+    // Pipe req
+    req.pipe(busboyInstance);
+});
+
+app.listen(PORT, () => {
+    console.log('Server running at port', PORT);
+})
